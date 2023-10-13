747ee36f7fb9af82c23875624c3ea08688bfc4ba
saul201287 <jssaul99@gmail.com>
2023-10-13 14:55:09 -0600

    prueba

--- backend/API/Test/colecciones.routes.js
+++ backend/API/Test/colecciones.routes.js
@@ -18,6 +18,7 @@ describe("Pruebas sobre la API de trips", () => {
       response = await request(app).get("/api/trips").send();
     });
 
+    
     it("La ruta funciona", async () => {
       expect(response.status).toBe(200);
       expect(response.headers["content-type"]).toContain("json");
