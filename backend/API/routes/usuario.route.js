dbd552c3b04a5e20a31f0d2152deafa5fec8c284
JAL117 <106935663+JAL117@users.noreply.github.com>
2023-10-13 14:32:43 -0600

    Creacion de las rutas para clientes

--- backend/API/routes/usuario.route.js
+++ backend/API/routes/usuario.route.js
@@ -96,3 +96,58 @@ router.delete("/:usuarioId", async (req, res) => {
     res.status(500).json({ error: "Ha ocurrido un error" });
   }
 });
+
+
+/* Clientes */
+router.get("/clientes", async (req, res) => {
+  try {
+    const cliente = await Cliente.find();
+    res.json(cliente);
+    console.log(cliente);
+  } catch (error) {
+    res.status(500).json({ error: "Ha ocurrido un error" });
+  }
+});
+
+router.post("/clientes", async (req, res) => {
+  try {
+    const newCliente = await Cliente.create(req.body);
+    res.json(newCliente);
+  } catch (error) {
+    res.status(500).json({ error: "Ha ocurrido un error" });
+  }
+});
+
+router.put("/clientes/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { nombre, telefono, referencia } = req.body;
+    Cliente.updateMany(
+      {
+        Cliente_id: id,
+      },
+      {
+        $set: {
+          nombre: nombre,
+          telefono: telefono,
+          referencia: referencia,
+        },
+      }
+    )
+      .then((data) => res.json(data))
+      .catch((error) => res.json({ message: error }));
+  } catch (error) {
+    res.status(500).json({ error: "Ha ocurrido un error" });
+  }
+});
+
+router.delete("/clientes/:clienteId", async (req, res) => {
+  try {
+    const cliente = await Cliente.findByIdAndDelete(req.params.clienteId);
+    res.json(cliente);
+  } catch (error) {
+    res.status(500).json({ error: "Ha ocurrido un error" });
+  }
+});
+module.exports = router;
+
